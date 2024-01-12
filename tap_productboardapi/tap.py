ec9bda6957218df182ddb5d45c168dacc0d583a5
jennyfaig <jenny.faig@immuta.com>
2024-01-12 13:11:18 -0500

    take bearer token value out of file

--- tap_productboardapi/tap.py
+++ tap_productboardapi/tap.py
@@ -20,7 +20,6 @@ class TapProductboardAPI(Tap):
             "api_key",
             th.StringType,
             required=True,
-            default="Bearer eyJ0eXAiOiJKV1QiLCJraWQiOiJhNDljZmViMmE3MmIyOTEzNzc1YTdjZDE4YzEyNmQxN2FjZjJiZjU0OWIzZTMxOGE4ZDgwZjk0NDhkZjlkYWU3IiwiYWxnIjoiUlM1MTIifQ.eyJpYXQiOjE2Nzg0NzQ3NTEsImlzcyI6IjA5ZWI0ZjA3LWMwNTItNGFhYy04MzhlLTExMjY5ZTlhNmNlOCIsInN1YiI6IjYzOTc2MyIsInJvbGUiOiJhZG1pbiIsImF1ZCI6Imh0dHBzOi8vYXBpLnByb2R1Y3Rib2FyZC5jb20iLCJ1c2VyX2lkIjo2Mzk3NjMsInNwYWNlX2lkIjoiMTQxOTcyIn0.U05bRQ6xcI-wKGCnXcpxtDa5tjj0VNfmPdsMScG6DYnTf-aG6WiA6PZ06qku4i8U9vy4Apy0ARulv2BKg0y-ok2g542kKktUOlFEQEdKbaUhhktknUb2Kw99HFWIUkGhle0EiHBTs5LxJUMruQVEkHiJxyGjSTN_PgVJK7xnD1zaQZMMNGY5QvkbFr3lVHLHcyrJZROFl7iwcQBsDFZAcJKPGAOtQjOSlpmL60BwFAHhwFT4upeTw-AYr3yavAQwjK_ubQBE4JvrzKLy2SKw2chbwi0s_IMu0fRjld3oqcrpE7E9LeLrh9IvmTjmkPutdFvnIL5-QFprCAohPMhw0w",
             secret=True,  # Flag config as protected.
             description="The token to authenticate against the API service",
         ),
